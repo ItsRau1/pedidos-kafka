9786797788942db45cd6986a36d967cdfee61182
Ruan Dias <ruan.dias77@outlook.com>
2025-09-18 15:33:24 -0300

    feat: add application enviorment files and run class in notificacao service

---
+++ src/notificacao-service/src/main/java/com/poc/notificacao_service/KafkaNotificacaoApplication.java
@@ -0,0 +1,13 @@
+package com.poc.notificacao_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaNotificacaoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaNotificacaoApplication.class, args);
+    }
+
+}
